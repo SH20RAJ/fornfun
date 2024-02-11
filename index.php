8847f1ccf8348c2d39109deb2dfa75d942710f34
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2024-02-11 14:17:26 +0530

    components

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-.....
+<?php
+echo "<!DOCTYPE html><html lang='en'><head>";
+include 'components/head.php';
+echo "</head><body>";
+
+include 'components/nav.php';
+include 'components/sidebar.php';
+include 'components/container.php';
+include 'components/footer.php';
+
+
+
+
+echo "</body></html>";
+?>
